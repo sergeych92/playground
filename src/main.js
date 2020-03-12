f401db4e55feb6322f368ab6ac7c72bef7dac507
Sergei Cherednichenko <sergeych92@gmail.com>
2020-03-12 23:21:11 +0100

    convert binary algorithm to strings

--- src/main.js
+++ src/main.js
@@ -14,36 +14,41 @@ function showFloat(bytes) {
     console.log(`fraction: ${str.substring(9)}`);
 }
 
-function binaryToDecimal(main, fraction, power) {
-    // Convert fraction to a decimal fraction
-    let decimalFrac = 0;
-    if (fraction > 0) {
-        // Find the position of the highest 1 (leftmost)
-        let digitMask = ~1;
-        while (fraction !== 0 && (fraction & digitMask) !== 0) {
-            digitMask <<= 1;
-        }
-        digitMask = (~digitMask + 1) >>> 1;
+// 101.1101 * 2^3
+function binaryToDecimal(numStr, power) {
+    const [_, mainStr, fractionStr] = /^([01]+)(?:\.([01]+))?$/.exec(numStr);
+    power = Math.floor(power);
 
-        // Sum the binary digits from left to right multiplied by 0.5
+    const main = parseInt(mainStr, 2);
+
+    // Convert fraction to a decimal fraction
+    let fraction = 0;
+    if (fractionStr) {
         let sum = 0;
         let divisor = 1/2;
-        while (digitMask > 0) {
-            const nextDigit = (fraction & digitMask) ? 1 : 0;
-            sum += divisor * nextDigit;
+        for (let i = 0; i < fractionStr.length; i++) {
+            sum += +fractionStr[i] * divisor;
             divisor /= 2;
-            digitMask >>>= 1;
         }
-        decimalFrac = sum;
+        fraction = sum;
     }
-    return (main + decimalFrac) * (2 ** power);
+    
+    return (main + fraction) * (2 ** power);
 }
 
-// float32[0] = 13;
-// showFloat(uint8);
+// float32[0] = binaryToDecimal('0.1', -130);
+// sign: 0
+// power: 00000000
+// fraction: 00001000000000000000000
+
+
+// float32[0] = binaryToDecimal('1.1', -130);
+// sign: 0
+// power: 00000000
+// fraction: 00011000000000000000000
+
+showFloat(uint8);
 // 0.1101 == 0.8125
-const bn = binaryToDecimal(1, 0b01101, 0);
-console.log(`number: ${bn}`);
 
 
 function convertToFloatManually(num) {
