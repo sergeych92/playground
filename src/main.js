c3acbe895bafae294b6b20c69592d176ad966007
Sergei Cherednichenko <sergeych92@gmail.com>
2020-03-12 17:23:43 +0100

    fix decimal conversion

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import '../css/style.scss';
+
 const mem = new ArrayBuffer(4);
 const float32 = new Float32Array(mem);
 const uint8 = new Uint8Array(mem);
@@ -13,10 +14,6 @@ function showFloat(bytes) {
     console.log(`fraction: ${str.substring(9)}`);
 }
 
-float32[0] = 13;
-showFloat(uint8);
-
-
 function binaryToDecimal(main, fraction, power) {
     // Convert fraction to a decimal fraction
     let decimalFrac = 0;
@@ -30,15 +27,24 @@ function binaryToDecimal(main, fraction, power) {
 
         // Sum the binary digits from left to right multiplied by 0.5
         let sum = 0;
+        let divisor = 1/2;
         while (digitMask > 0) {
-            const nextDigit = fraction & digitMask;
-            sum = 0.5 * (nextDigit + sum);
+            const nextDigit = (fraction & digitMask) ? 1 : 0;
+            sum += divisor * nextDigit;
+            divisor /= 2;
             digitMask >>>= 1;
         }
         decimalFrac = sum;
     }
     return (main + decimalFrac) * (2 ** power);
 }
+
+// float32[0] = 13;
+// showFloat(uint8);
+// 0.1101 == 0.8125
+const bn = binaryToDecimal(1, 0b01101, 0);
+console.log(`number: ${bn}`);
+
 
 function convertToFloatManually(num) {
 
