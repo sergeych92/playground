653c04320eeac9bc3b17d162fe4d5fdf60dcefcf
Sergei Cherednichenko <sergeych92@gmail.com>
2020-05-01 18:56:33 +0200

    math modeling

--- src/longest-sequence-bits.js
+++ src/longest-sequence-bits.js
@@ -68,3 +68,32 @@ function longestOnesSequence(n) {
 // if (!failed) {
 //     console.log('PASSED');
 // }
+
+
+
+// Draw a line in a screen of bit pixels
+function drawLine(bytes, width, x1, x2, y) {
+    const row = width * y >> 3;
+    const x1b = x1 >> 3;
+    const x2b = x2 >> 3;
+    for (let i = x1b + 1; i < x2b; i++) {
+        bytes[row + i] = 0xFF;
+    }
+    const left = (1 << (8 - x1 % 8)) - 1;
+    const right = ((-1 >>> 0) << (7 - x2 % 8)) & 0xFF;
+    if (x1b === x2b) {
+        bytes[row + x1b] = left & right;
+    } else {
+        bytes[row + x1b] = left;
+        bytes[row + x2b] = right;
+    }
+}
+
+
+const bytes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// drawLine(bytes, 32, 35, 54, 0); // 3 & 22
+drawLine(bytes, 32, 3, 15, 1); // 3 & 22
+for (let i = 0; i < bytes.length / 4; i++) {
+    const toStr = n => n.toString(2).padStart(8, '0');
+    console.log(`${toStr(bytes[i*4])}  ${toStr(bytes[i*4 + 1])}  ${toStr(bytes[i*4 + 2])}  ${toStr(bytes[i*4 + 3])}`);
+}
--- src/main.js
+++ src/main.js
@@ -1,27 +1,84 @@
 import '../css/style.scss';
 
-function drawLine(bytes, width, x1, x2, y) {
-    const row = width * y >> 3;
-    const x1b = x1 >> 3;
-    const x2b = x2 >> 3;
-    for (let i = x1b + 1; i < x2b; i++) {
-        bytes[row + i] = 0xFF;
+
+function sum4Formula(n) {
+    const sum = (6 * n ** 5 + 15 * n ** 4 + 10 * n ** 3 - n) / 30;
+    console.log(`sum of 1 to ${n} is ${sum}`);
+}
+
+function sum4(n) {
+    let s = 0;
+    for (let x = 1; x <= n; x++) {
+        s += x ** 4;
     }
-    const left = (1 << (8 - x1 % 8)) - 1;
-    const right = ((-1 >>> 0) << (7 - x2 % 8)) & 0xFF;
-    if (x1b === x2b) {
-        bytes[row + x1b] = left & right;
-    } else {
-        bytes[row + x1b] = left;
-        bytes[row + x2b] = right;
+    console.log(`sum of 1 to ${n} is: ${s}`);
+}
+
+function sumXSquared(a) {
+    const n = 1000;
+    const dx = a / n;
+    let s = 0;
+    for (let i = 1; i <= n; i++) {
+        s += dx * (dx * i) ** 2;
     }
+    console.log(`sum is ${s}`);
+}
+
+function max(a, b, c) {
+    if (b * b <= 3 * a * c) {
+        console.log('No roots');
+    }
+    const v = -b / (3 * a) - Math.sqrt(b * b - 3 * a * c) / (6 * a * a);
+    console.log(`max is ${v}`);
+}
+
+function max2(a, b, c) {
+    if (b * b <= 3 * a * c) {
+        console.log('No roots');
+    }
+    const v = -b / (3 * a) + Math.sqrt(b * b - 3 * a * c) / (6 * a * a);
+    console.log(`max is ${v}`);
 }
 
 
-const bytes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-// drawLine(bytes, 32, 35, 54, 0); // 3 & 22
-drawLine(bytes, 32, 3, 15, 1); // 3 & 22
-for (let i = 0; i < bytes.length / 4; i++) {
-    const toStr = n => n.toString(2).padStart(8, '0');
-    console.log(`${toStr(bytes[i*4])}  ${toStr(bytes[i*4 + 1])}  ${toStr(bytes[i*4 + 2])}  ${toStr(bytes[i*4 + 3])}`);
+
+function modelBirths(families) {
+    let girls = 0;
+    let boys = 0;
+    while (families > 0) {
+        let subtract = 0;
+        for (let i = 1; i <= families; i++) {
+            if (Math.random() >= 0.5) { // baby girl
+                girls++;
+                subtract++;
+            } else {
+                boys++;
+            }
+        }
+        families -= subtract;
+    }
+    const childrenTotal = girls + boys;
+    console.log(`children born: ${childrenTotal},
+        boys: ${(boys/childrenTotal).toFixed(2)},
+        girls: ${(girls/childrenTotal).toFixed(2)}`);
+}
+
+
+function countPermutations(total, selected) {
+    if (selected === 0) {
+        return 1;
+    } else if (selected === 1) {
+        return total;
+    } else {
+        let sum = 0;
+        for (let i = selected - 1; i <= total - 1; i++) {
+            sum += countPermutations(i, selected - 1);
+        }
+        return sum;
+    }
+}
+
+for (let i = 0; i <= 10; i++) {
+    const probability = (3 ** i) / (4 ** 10) * countPermutations(10, i);
+    console.log(`select ${i} out of ${10}: ${probability}`);
 }
