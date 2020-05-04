e5027150e0d989dfed90a050a94e784c95bb1783
Sergei Cherednichenko <sergeych92@gmail.com>
2020-05-04 18:21:26 +0200

    find poisoned bottle

--- src/main.js
+++ src/main.js
@@ -78,7 +78,73 @@ function countPermutations(total, selected) {
     }
 }
 
-for (let i = 0; i <= 10; i++) {
-    const probability = (3 ** i) / (4 ** 10) * countPermutations(10, i);
-    console.log(`select ${i} out of ${10}: ${probability}`);
+// for (let i = 0; i <= 10; i++) {
+//     const probability = (3 ** i) / (4 ** 10) * countPermutations(10, i);
+//     console.log(`select ${i} out of ${10}: ${probability}`);
+// }
+
+
+
+
+function computeMaxCapacity(tests, roundsLeft) {
+    if (roundsLeft <= 0) {
+        return 1;
+    } else if (roundsLeft === 1) {
+        return tests + 1;
+    } else if (roundsLeft === 2) {
+        return tests ** 2 + tests + 1;
+    } else if (roundsLeft === 3) {
+        return tests ** 3 + 2 * tests + 1;
+    } else {
+        let sum = 0;
+        for (let i = 0; i <= roundsLeft - 1; i++) {
+            sum += tests * computeMaxCapacity(tests - 1, i);
+        }
+        return sum + 1;
+    }
 }
+
+function isBatchPoisonous(bottles, left, right) {
+    for (let i = left; i <= right; i++) {
+        if (bottles[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function findPoisonedBottle(bottles, tests, left, right) {
+    const bottlesCount = right - left + 1;
+    if (bottlesCount <= tests + 1) {
+        for (let i = left; i <= right; i++) {
+            if (bottles[i]) {
+                console.log(`test size: ${tests}`);
+                return i;
+            }
+        }
+        console.log(`test size: ${tests}`);
+        return right + 1;
+    } else {
+        let depestLevel = 1;
+        while (computeMaxCapacity(tests - 1, depestLevel) * tests < bottlesCount) {
+            depestLevel++;
+        }
+        const nextBatchSize = computeMaxCapacity(tests - 1, depestLevel - 1);
+
+        for (let i = 0; i < tests; i++) {
+            const subLeft = left + nextBatchSize * i;
+            const subRight = left + nextBatchSize * (i + 1) - 1;
+            const found = isBatchPoisonous(bottles, subLeft, subRight);
+            if (found) {
+                return findPoisonedBottle(bottles, tests - 1, subLeft, subRight);
+            }
+        }
+    
+        return findPoisonedBottle(bottles, tests, left + nextBatchSize * tests, right);
+    }    
+}
+
+const bottles = [...'0'.repeat(1000)].map(c => !!+c);
+bottles[132] = true;
+const poisonedIndex = findPoisonedBottle(bottles, 10, 0, bottles.length - 1);
+console.log(`Spiked drink is in bottle ${poisonedIndex}`);
