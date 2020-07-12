26f40552298a688e8f0bb2939362d2dafe6c67b0
Sergei Cherednichenko <sergeych92@gmail.com>
2020-07-11 21:47:04 -0400

    find path between any two points

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import '../css/style.scss';
-import { buildTrie, findTypeaheadMatches } from './trie-typeahead';
+import { hasPathBetween } from './graphs';
 
-const trieRoot = buildTrie([
-    'cat',
-    'call',
-    'man',
-    'many',
-    'manual',
-    'solid'
-]);
+const a1 = {adj: []};
+const a2 = {adj: []};
+const a3 = {adj: []};
+const a4 = {adj: []};
+const a5 = {adj: []};
+const a6 = {adj: []};
+const a7 = {adj: []};
+const a8 = {adj: []};
 
-const matches = findTypeaheadMatches(trieRoot, 'c');
-for (let match of matches) {
-    console.log(match);
-}
+a1.adj.push(a2, a5);
+a2.adj.push(a3);
+a3.adj.push(a4);
+a4.adj.push(a2);
+a5.adj.push(a6, a7);
+a6.adj.push(a7);
+a7.adj.push(a5, a8);
+a8.adj.push(a4);
+
+const r = hasPathBetween(a3, a8);
+console.log(`Has path: ${r}`);
